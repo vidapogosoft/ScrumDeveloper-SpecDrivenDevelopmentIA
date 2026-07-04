cb1c2c4218718af4e4cb9bff059a1ca817b6313a
vidapogosoft <vidapogosoft@hotmail.com>
2026-07-04 14:10:22 -0500

    automatizaciones
    selenium - appium - winappdriver

---
+++ NetAppiumMobile1/UnitTest2.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Appium.Android;
+
+using System.Threading;
+
+namespace NetAppiumMobile1
+{
+    [TestClass]
+    public class UnitTest2
+    {
+
+        //crear la instancia para el appium driver
+
+        private AndroidDriver<AndroidElement> driverMobile;
+
+        [TestMethod]
+        public void TestInit()
+        {
+
+            DesiredCapabilities cap = new DesiredCapabilities();
+
+            cap.SetCapability("app", "D:\\vidapogosoft\\cursos\\CrudContact\\app-release.apk");
+            cap.SetCapability("device", "SM-T295");
+            cap.SetCapability("deviceName", "Tab VPR");
+            cap.SetCapability("platformName", "Android");
+
+
+
+            ///launch del android driver
+            driverMobile = new AndroidDriver<AndroidElement>(new Uri("http://127.0.0.1:4723/wd/hub"), cap);
+
+
+            //Encontrar elementos
+
+            Thread.Sleep(5000);
+
+            //Cajas de texto
+            AndroidElement TxtId = driverMobile.FindElementById("com.android.sqlite:id/TxtCodigo");
+            TxtId.SendKeys("100");
+
+            AndroidElement TxtDescripcion = driverMobile.FindElementById("com.android.sqlite:id/TxtDescripcion");
+            TxtDescripcion.SendKeys("Product Test Automation");
+
+            AndroidElement TxtPrecio = driverMobile.FindElementById("com.android.sqlite:id/TxtPrecio");
+            TxtPrecio.SendKeys("15");
+
+            Thread.Sleep(3000);
+
+            ///Boton
+            AndroidElement BtnSave = driverMobile.FindElementById("com.android.sqlite:id/BtnSave");
+            BtnSave.Click();
+
+            Thread.Sleep(3000);
+
+            AndroidElement Resultado = driverMobile.FindElementById("com.android.sqlite:id/TxtResultado");
+            
+            Assert.AreEqual("100-Product Test Automation", Resultado.Text);
+
+        }
+
+    }
+}
---
+++ NetWinAppDriver1/NetWinAppDriver1/UnitTest3.cs
@@ -0,0 +1,62 @@
+﻿using NUnit.Framework;
+
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+
+using System;
+
+namespace NetWinAppDriver1
+{
+    public class UnitTest3
+    {
+
+        private WindowsDriver<WindowsElement> _driver;
+
+        [SetUp]
+        public void TestInit()
+        {
+
+            var options = new AppiumOptions();
+            options.AddAdditionalCapability("app", "D:\\vidapogosoft\\cursos\\2026\\sinergiass\\automotors\\AppWinForm1.exe");
+            options.AddAdditionalCapability("deviceName", "PORTUGALASUS");
+
+            _driver = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), options);
+
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+        }
+
+        [TearDown]
+        public void TestCleanup()
+        {
+            if (_driver != null)
+            {
+
+                _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+
+                _driver.Quit();
+                _driver = null;
+            }
+        }
+
+        [Test]
+        public void TestWIndows()
+        {
+
+            _driver.FindElementByAccessibilityId("TxtIdentificacion").Click();
+            _driver.FindElementByAccessibilityId("TxtIdentificacion").SendKeys("0925183665");
+
+            _driver.FindElementByAccessibilityId("TxtNombres").Click();
+            _driver.FindElementByAccessibilityId("TxtNombres").SendKeys("Curso de Automatizacion WinAppDrive");
+
+
+            _driver.FindElementByAccessibilityId("TxtDireccion").Click();
+            _driver.FindElementByAccessibilityId("TxtDireccion").SendKeys("GYE Nor Oeste");
+
+
+            _driver.FindElementByName("Confirmar Datos").Click();
+
+        }
+
+    }
+}
